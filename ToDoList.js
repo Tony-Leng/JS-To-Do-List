71fb7ed5e6245e12b832881737a6c1955507cd51
Tony Leng <lengtk@gmail.com>
2023-03-01 15:25:25 +0900

    added title and options to html, enabled quit function

---
+++ ToDoList.js
@@ -0,0 +1,25 @@
+// While loop !== quit keep looping
+// Make quit part work first to avoid infinite loop
+// Use prompt
+// Storage of inputs in array, create at beginning in empty array named todolist, use .push
+// Conditional inside while loop
+// If input === new, then add input to array with push
+// If input === list, iterate over all todos and print out
+// Make sure list has indexes
+// If input === delete, use index w/ splice to remove
+
+// This will use
+// - Arrays
+// - Loops
+//    - For
+//    - While
+// - Conditional
+let toDoList = [];
+let input = prompt('What would you like to do?');
+
+if (input === 'quit') {
+  console.log('See ya!')
+}
+while (input !== 'quit') {
+
+}
